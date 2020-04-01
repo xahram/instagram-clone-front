83274abbfcd4cf9534d1932f105baaaebd178166
xahram <ssasuke1163@gmail.com>
2020-04-01 21:37:45 +0500

    Testing Deployment

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -24,6 +24,7 @@ export default class UserProfile extends React.Component {
                 console.log(err)
             })
     }
+    
     render() {
         return (<div>
             <UserProfilePic
